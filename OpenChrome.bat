465c25cb2aae20d458980d8e6f6bf091dcf9ae27
m.prashanth <prashanthmangena@gmail.com>
2022-04-10 03:42:53 +0530

    Initial commit

---
+++ OpenChrome.bat
@@ -0,0 +1,6 @@
+@echo off
+rem TO OPEN CHROME
+
+echo "Starting Run"
+start chrome --new-window "www.google.com"
+echo "Ran Successfully "
---
+++ StartStudy.bat
@@ -0,0 +1,15 @@
+@echo on
+
+echo "Starting Run"
+
+rem OPEN CHROME
+start chrome
+
+rem OPEN 5lb 
+start microsoft-edge: "file:C:\Users\m.prashanth\Downloads\5 lb. Book of GRE Practice Problems 3rd Edition.pdf"
+
+start calc
+
+start explorer.exe shell:Appsfolder\Microsoft.WindowsAlarms_8wekyb3d8bbwe!App
+
+echo "Ran Successfully "
