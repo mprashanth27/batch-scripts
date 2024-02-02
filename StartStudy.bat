a28ae71b239989000d27397bd68c6af705655965
m.prashanth <prashanthmangena@gmail.com>
2024-02-01 20:50:56 -0700

    Replaced Chrome with Edge for music

--- StartStudy.bat
+++ StartStudy.bat
@@ -8,16 +8,17 @@ start chrome --profile-directory="Profile 1"
 rem OPEN CHROME -Mine
 start chrome --profile-directory="Default"
 
-rem OPEN CHROME -Music
-start chrome "https://www.youtube.com/watch?v=PsO6ZnUZI0g" --incognito
+rem OPEN Edge -Music
+start msedge "https://www.youtube.com/watch?v=PsO6ZnUZI0g" -inprivate
 
 
 #start calc
 
+rem open Clock
 start explorer.exe shell:Appsfolder\Microsoft.WindowsAlarms_8wekyb3d8bbwe!App
 
-start cmd /k "cd F:\Prashanth disk(f)\ASU" #Not gng to the Dir.
+#start cmd /k "cd F:\Prashanth disk(f)\ASU" #Not gng to the Dir.
 
-start explorer "cd F:\Prashanth disk(f)\ASU" #Not gng to the Dir.
+start explorer
 
 echo "Ran Successfully "
